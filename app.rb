aef27f662d050a830acf42122a42c8c389d5d9fb
Ariel I <arielibarra39@gmail.com>
2020-10-25 03:03:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
   post '/checkout' do 
     @sessions = session
     @item = params[:item]
-    @session[:item]
+    @session[:item] = item
     
     erb :checkout
   end 
